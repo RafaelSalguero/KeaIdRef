c1f735e9c8de1c4d0564d695a1347edfbae938e4
Rafael Salguero Iturrios <rafaelsalgueroiturrios@gmail.com>
2015-12-15 15:25:48 -0700

    Kea.Config

---
+++ KeaIdRef/Kea.Config/IConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kea.Interfaces
+{
+    /// <summary>
+    /// A service that gets and sets values by string keys
+    /// </summary>
+    public interface IConfig
+    {
+        /// <summary>
+        /// Set a value by its key
+        /// </summary>
+        /// <param name="Key"></param>
+        /// <param name="Value"></param>
+        void Set(string Key, string Value);
+        /// <summary>
+        /// Gets a value by its key, returns null if not found
+        /// </summary>
+        /// <param name="Key"></param>
+        /// <returns></returns>
+        string Get(string Key);
+    }
+}
---
+++ KeaIdRef/Kea.Config/InMemoryConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kea.Interfaces
+{
+    /// <summary>
+    /// Implements an in-memory dictionary as IConfig service 
+    /// </summary>
+    public class InMemoryConfig : IConfig
+    {
+        Dictionary<string, string> data = new Dictionary<string, string>();
+        string IConfig.Get(string Key)
+        {
+            string result;
+            if (data.TryGetValue(Key, out result))
+                return result;
+            else
+                return null;
+        }
+
+        void IConfig.Set(string Key, string Value)
+        {
+            data[Key] = Value;
+        }
+    }
+}
