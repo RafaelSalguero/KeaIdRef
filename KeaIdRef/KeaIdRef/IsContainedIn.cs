862c365efb8d8899b92c98f4ac760abf6fee1fe4
Rafael Salguero Iturrios <rafaelsalgueroiturrios@gmail.com>
2015-12-15 11:39:14 -0700

    initial commit

---
+++ KeaIdRef/KeaIdRef/IsContainedIn.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kea.Serialization
+{
+    /// <summary>
+    /// Indicate that the value of this property is contained on the given collection
+    /// </summary>
+    [System.AttributeUsage(AttributeTargets.Property, Inherited = true, AllowMultiple = false)]
+    public sealed class IsContainedInAttribute : Attribute
+    {
+        readonly string collectionProperty;
+
+        /// <summary>
+        /// Indicate that the value of this property is contained on the given collection
+        /// </summary>
+        /// <param name="collectionProperty"></param>
+        public IsContainedInAttribute(string collectionProperty)
+        {
+            this.collectionProperty = collectionProperty;
+        }
+
+        /// <summary>
+        /// The name of the collection property that contains this property value
+        /// </summary>
+        public string CollectionProperty
+        {
+            get { return collectionProperty; }
+        }
+    }
+}
